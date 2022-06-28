dcc7c59521ce58878429a3c2f20a599b0e5339ff
Lisa Williams <lisaismyname0@gmail.com>
2022-06-28 11:15:42 -0400

    registered Note on admin site

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Contact
 from .models import Favorite
+from .models import Note
 
 admin.site.register(Contact)
 # MODEL NAMES ARE ALWAYS SINGULAR
 admin.site.register(Favorite)
+admin.site.register(Note)
--- contacts/forms.py
+++ contacts/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Contact
-from .models import Note
 
 
 class ContactForm(forms.ModelForm):
